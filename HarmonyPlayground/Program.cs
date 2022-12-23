8fa227e92d62d82aa4348a8636a625d997f30e8e
Anna Vasenina <avasenina97@gmail.com>
2022-12-23 14:48:58 +0300

    patch with reflection

--- HarmonyPlayground/Program.cs
+++ HarmonyPlayground/Program.cs
@@ -1,14 +1,41 @@
 ﻿using System.Diagnostics;
+using System.Reflection.Emit;
+using HarmonyLib;
+
+class ppp
+{
+    private static List<string> ress = new List<string>() { "11", "222" };
+        
+    static void MyPostfix(ref string __result)
+    {
+        __result = ress[0];
+        ress.RemoveAt(0);
+    }
+
+}
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main1(string[] args)
     {
         MyPatcher.DoPatching();
         var r1 = DtNow();
         var r2 = StrDt2013();
         Debug.Assert(r1 == new DateTime(2011, 1,1));
         Debug.Assert(r2 == "patched");
+    }
+    
+    static void Main(string[] args)
+    {
+        var harmony = new Harmony("com.example.patch");
+
+        var mOriginal = AccessTools.Method(typeof(DateTime), "ToLongDateString"); // if possible use nameof() here
+        var mPostfix = AccessTools.Method(typeof(ppp), "MyPostfix");
+
+        harmony.Patch(mOriginal, postfix: new HarmonyMethod(mPostfix));
+        Console.WriteLine(StrDt2013());
+        Console.WriteLine(StrDt2013());
+        Console.ReadLine();
     }
 
     static private DateTime DtNow()
