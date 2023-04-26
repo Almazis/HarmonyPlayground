6078829d19f60d933a7e103b453d2d68e564b97e
Anna Vasenina <avasenina97@gmail.com>
2023-04-26 16:55:12 +0300

    Monomod extern patches

--- HarmonyPlayground/Program.cs
+++ HarmonyPlayground/Program.cs
@@ -1,6 +1,10 @@
-﻿using System.Diagnostics;
-using System.Reflection.Emit;
-using HarmonyLib;
+﻿using MonoMod.RuntimeDetour;
+using MonoMod.Utils;
+using System;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.ComponentModel;
 
 class ppp
 {
@@ -12,102 +16,67 @@ class ppp
         counter++;
     }
 
-    static string MyExtraMethod()
+    static public string MyExtraMethod()
     {
         var res = ress[counter];
         counter++;
         return res;
     }
-    static System.Reflection.MethodInfo m_MyExtraMethod = SymbolExtensions.GetMethodInfo(() => MyExtraMethod());
-    static IEnumerable<CodeInstruction> MyTranspiler(IEnumerable<CodeInstruction> instructions)
-    {
-        yield return new CodeInstruction(OpCodes.Call, m_MyExtraMethod);
-        yield return new CodeInstruction(OpCodes.Ret);
-    }
-}
-
-class NowPatch
-{
-    private static DateTime[] ress = new DateTime[] {new DateTime(2012, 1, 1), new DateTime(2007,12,1)};
-    private static int counter = 0;
-    static void MyPostfix(ref DateTime __result)
-    {
-        __result = ress[counter];
-        counter++;
-    }
-
 }
 
 class Program
 {
     static void Main1(string[] args)
     {
-        MyPatcher.DoPatching();
-        var r1 = DtNow();
-        var r2 = StrDt2013();
-        Debug.Assert(r1 == new DateTime(2011, 1,1));
-        Debug.Assert(r2 == "patched");
-    }
-    
-    static void Main2(string[] args)
-    {
-        var harmony = new Harmony("com.example.patch");
 
-        var mOriginal = AccessTools.Method(typeof(DateTime), "ToLongDateString"); // if possible use nameof() here
-        var mPostfix = AccessTools.Method(typeof(ppp), "MyPostfix");
+        IDetour detour1 = new Detour(
+                    () => default(DateTime).ToLongDateString(),
+                    () => ppp.MyExtraMethod()
+        );
+        detour1.GenerateTrampoline<Action<string, string>>();
+        var s1 = StrDt2013();
+        var s2 = StrDt2013();
 
-        harmony.Patch(mOriginal, postfix: new HarmonyMethod(mPostfix));
+        Console.WriteLine(s1);
+        Console.WriteLine(s2);
+
+        detour1.Undo();
         Console.WriteLine(StrDt2013());
-        Console.WriteLine(StrDt2013());
-        Console.ReadLine();
-    }
-
-    static void Main3(string[] args)
-    {
-        DateTime dtMain = new DateTime(2012, 12, 12);
-        var harmony = new Harmony("com.example.patch");
-        
-        var mOriginal = SymbolExtensions.GetMethodInfo(() => dtMain.ToLongDateString());
-        var mPostfix = AccessTools.Method(typeof(ppp), "MyPostfix");
-
-        harmony.Patch(mOriginal, postfix: new HarmonyMethod(mPostfix));
-        Console.WriteLine(dtMain.ToLongDateString());
-        Console.WriteLine(dtMain.ToLongDateString());
-
-        Console.WriteLine(StrDt2013());
-        Console.ReadLine();
-    }
-    
-    static void Main4(string[] args)
-    {
-        var harmony = new Harmony("com.example.patch");
-        
-        // var mOriginal = SymbolExtensions.GetMethodInfo(() => DtNow());
-        var mOriginal = AccessTools.PropertyGetter(typeof(DateTime), "Now");
-
-        var mPostfix = AccessTools.Method(typeof(NowPatch), "MyPostfix");
-
-        harmony.Patch(mOriginal, postfix: new HarmonyMethod(mPostfix));
-        Console.WriteLine(DtNow());
-        Console.WriteLine(DtNow());
-
-        // Console.WriteLine(StrDt2013());
         Console.ReadLine();
     }
     
     static void Main(string[] args)
     {
-        DateTime dtMain = new DateTime(2012, 12, 12);
-        var harmony = new Harmony("com.example.patch");
+        MethodBase bTo = typeof(Program).GetMethod("not_rand", BindingFlags.Static | BindingFlags.Public);
+        IntPtr ptrTo = bTo.MethodHandle.GetFunctionPointer();
+        typeof(Program).GetMethod("not_rand", BindingFlags.Static | BindingFlags.Public,
+                                                null, new[] { typeof(void) }, null);
 
-        var mOriginal = SymbolExtensions.GetMethodInfo(() => dtMain.ToLongDateString());
-        var mPostfix = AccessTools.Method(typeof(ppp), "MyTranspiler");
+        DynDll.TryOpenLibrary("libc.so.6", out IntPtr libc);
+            // !(PlatformHelper.Is(Platform.MacOS) && DynDll.TryOpenLibrary("/usr/lib/libc.dylib", out libc)) &&
+            // !DynDll.TryOpenLibrary($"libc.{PlatformHelper.LibrarySuffix}", out libc))
+            // return;
+
+        NativeDetour d = new NativeDetour(
+            libc.GetFunction("rand"),
+            ptrTo,
+            new NativeDetourConfig() {
+                ManualApply = false
+            }
+        );
+        var v1 = libc_rand();
+        var v2 = libc_rand();
+        var v3 = libc_rand();
+        var v4 = libc_rand();
+
+        Console.WriteLine(v1);
+        Console.WriteLine(v2);
+        Console.WriteLine(v3);
+        Console.WriteLine(v4);
         
-        harmony.Patch(mOriginal, transpiler: new HarmonyMethod(mPostfix));
-
-        Console.WriteLine(dtMain.ToLongDateString());
-        Console.WriteLine(dtMain.ToLongDateString());
-
+        d.Undo();
+        Console.WriteLine(libc_rand());
+        
         Console.ReadLine();
     }
     
@@ -122,5 +91,10 @@ class Program
         return dt.ToLongDateString();
     }
 
+    public static int not_rand() {
+        return -1;
+    }
+    [DllImport("libc", EntryPoint = "rand", CallingConvention = CallingConvention.Cdecl)] 
+    public static extern int libc_rand();
 }
     
